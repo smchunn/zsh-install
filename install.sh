f2a3d4d4a96647167bf39bdb4f6d048b8c79ea3d
Spencer Chunn <smcnola1@gmail.com>
2022-01-06 21:06:35 -0600

    fix in script

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 curl https://raw.githubusercontent.com/smchunn/zsh-install/main/smchunn.zsh-theme -o ~/.oh-my-zsh/custom/themes/smchunn.zsh-theme
 
-sed -i '.bak' 's/^ZSH_THEME.*/ZSH_THEME="smchunn"/' ~/.zshrc
+sed -i.bak 's/^ZSH_THEME.*/ZSH_THEME="smchunn"/' ~/.zshrc
