8627abe76d083eb13d7c3ea300a58ed78161e73c
Spencer Chunn <smcnola1@gmail.com>
2022-01-12 17:45:12 -0600

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-
+if                                                        17:42:33
+[ ! -d /home/smcadmin/.oh-my-zsh ]
+then
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
 curl https://raw.githubusercontent.com/smchunn/zsh-install/main/smchunn.zsh-theme -o ~/.oh-my-zsh/custom/themes/smchunn.zsh-theme
 
-sed -i.bak 's/^ZSH_THEME.*/ZSH_THEME="smchunn"/' ~/.zshrc
+sed -i 's/^ZSH_THEME.*/ZSH_THEME="smchunn"/' ~/.zshrc
